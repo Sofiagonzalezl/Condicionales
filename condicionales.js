92a026767835683a2f8ca7eea58eb1b0d8213275
Sofiagonzalezl <92395715+Sofiagonzalezl@users.noreply.github.com>
2022-03-08 15:04:58 -0500

    Update condicionales.js

--- condicionales.js
+++ condicionales.js
@@ -1,13 +1,24 @@
 let urlfeliz = "https://thumbs.dreamstime.com/b/emoticon-con-la-cara-feliz-75511442.jpg" 
 let urltriste = "https://i.pinimg.com/originals/c3/88/11/c38811fd35dd552cbf457b54b5fb85f4.jpg"
 let imgHtml = document.querySelector ("#imagencara")
-let nota1 = prompt ("Ingrese su primera nota")
-let nota2 = prompt ("Ingrese su segunda nota")
-let nota3 = prompt ("Ingrese su tercera nota")
+let nota1
+
+do{
+   nota1 = Number(prompt ("Ingrese su primera nota"))
+}while(isNaN(nota1) || nota1> 10 || nota1< 0);
+
+do{
+   nota2 = Number(prompt ("Ingrese su primera nota"))
+}while(isNaN(nota2) || nota2> 10 || nota2< 0);
+
+do{
+   nota3 = Number(prompt ("Ingrese su primera nota"))
+}while(isNaN(nota3) || nota3> 10 || nota3< 0);
+
 let p = (parseFloat(nota1)+parseFloat(nota2)+parseFloat(nota3))/3;
 alert("Su promedio es: " +p)
-if(p >= "6")﹛
+if(p >= "6"){
    imgHtml.src = urlfeliz 
-﹜else﹛
+}else{
    imgHtml.src = urltriste
-   ﹜
+}
